db7997eb2e5408b78b678ed3588a920c25c6d809
ecor <emanuele.cordano@gmail.com>
2014-12-22 16:39:01 +0100

    adding is.raster variable

--- R/listClimApply.R
+++ R/listClimApply.R
@@ -20,10 +20,19 @@ NULL
 listClimApply <- function(P=NA,Tx=NA,Tm=NA,Tn=NA,month=1:12,clim_fun=contin,
 		latitude,longitude,elevation,coeff_rad=NA,...)   {
 	
+	##### 
+	is.raster <- FALSE  
+	if (class(P)=="RasterStack" | class(P)=="RasterBrick") is.raster <- TRUE
+	if (class(Tx)=="RasterStack" | class(Tx)=="RasterBrick" | is.na(Tx)) is.raster <- is.raster & TRUE
+	if (class(Tm)=="RasterStack" | class(Tm)=="RasterBrick" | is.na(Tm)) is.raster <- is.raster & TRUE
+	if (class(Tn)=="RasterStack" | class(Tn)=="RasterBrick" | is.na(Tn)) is.raster <- is.raster & TRUE	
+	
+	
+	
 	
 
 	out <- mapply(month=month,P=P,Tx=Tx,Tm=Tm,Tn=Tn,latitude=latitude,
-			longitude=longitude,elevation=elevation,coeff_rad=coeff_rad,clim_fun=clim_fun,FUN=climAppy,...)
+			longitude=longitude,elevation=elevation,coeff_rad=coeff_rad,clim_fun=clim_fun,FUN=climApply,...)
 	
 	
 	
